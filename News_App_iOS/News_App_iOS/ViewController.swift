62726de0028abcc6d317d7922043309008cb6e22
rushyanth1130 <s543652@nwmissouri.edu>
2022-04-04 13:31:56 -0500

    Initial project setup

---
+++ News_App_iOS/News_App_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News_App_iOS
+//
+//  Created by Nagarushyanth Tummala on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
