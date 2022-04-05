7c04308db6439b141b892d73307b1fd703871075
rushyanth1130 <s543652@nwmissouri.edu>
2022-04-05 13:35:32 -0500

    tab bar

---
+++ News_App_iOS/News_App_iOS/mainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  mainViewController.swift
+//  News_App_iOS
+//
+//  Created by Nagarushyanth Tummala on 4/5/22.
+//
+
+import UIKit
+
+class mainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
