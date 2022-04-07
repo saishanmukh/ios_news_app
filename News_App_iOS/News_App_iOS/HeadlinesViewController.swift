18403134838aecf0fcfa1562e39b8bba93d7bce5
rushyanth1130 <85264598+rushyanth1130@users.noreply.github.com>
2022-04-06 22:31:34 -0500

    new view controllers added

---
+++ News_App_iOS/News_App_iOS/HeadlinesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeadlinesViewController.swift
+//  News_App_iOS
+//
+//  Created by student on 4/6/22.
+//
+
+import UIKit
+
+class HeadlinesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
