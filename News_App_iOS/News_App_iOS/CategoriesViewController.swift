8dc595d0e1e80d50d91d870dcbcc03d6c6feee39
Chaitanya Punaati <89426302+ChaitanyaPunaati@users.noreply.github.com>
2022-05-01 20:29:34 -0500

    changes in category view

--- News_App_iOS/News_App_iOS/CategoriesViewController.swift
+++ News_App_iOS/News_App_iOS/CategoriesViewController.swift
@@ -7,19 +7,33 @@
 
 import UIKit
 
-class CategoriesViewController: UIViewController {
-
-    @IBOutlet weak var headlines: UITableView!
-    @IBAction func searchButton(_ sender: Any) {
+class CategoriesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categories.count
     }
     
-    @IBOutlet weak var categoryName: UITextField!
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = categoryCollectionView.dequeueReusableCell(withReuseIdentifier: "categoryCell", for: indexPath) as! CategoryCollectionViewCell
+        
+        cell.categoryNameOutlet.text = categories[0]
+        return cell
+    }
+    
+    
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    @IBOutlet weak var headlines: UITableView!
+    
+
+    let categories = ["Sports","Crime","Technology","Weather"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        categoryCollectionView.delegate = self
+        categoryCollectionView.dataSource = self
     }
     
-
 
 }
---
+++ News_App_iOS/News_App_iOS/CategoryCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCollectionViewCell.swift
+//  News_App_iOS
+//
+//  Created by student on 5/1/22.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var categoryNameOutlet: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    @IBOutlet weak var categoryView: UIView!
+    
+    
+}
